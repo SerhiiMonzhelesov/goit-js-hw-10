a31c3b841179d8bc1cd257ed827cc3913069eae4
SerhiiMonzhelesov <monzhelesov@gmail.com>
2023-06-21 00:24:11 +0300

    js hw10

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,22 +1,21 @@
 const BASE_URL = 'https://api.thecatapi.com/v1';
-const RESOURSE = '/breeds';
+const RESOURSE_BREEDS = '/breeds';
+const RESOURSE_CAT = '/images/search';
 const API_KEY = 'live_1yg9qODu4yV9MglnZAshOsC3MU3jMs7B2uLAwCvuWWNGCWPQR7RIr7cBASyTb7xg ';
 
-
 function fetchBreeds() {
-    const url = `${BASE_URL}${RESOURSE}?api_key=${API_KEY}`;
-    
+    const url = `${BASE_URL}${RESOURSE_BREEDS}?api_key=${API_KEY}`;
     return fetch(url).then((response) => {
-        if (!response.ok) {
-            throw new Error(error)
-        }
-        return response.json();
-    });
+        return response.json()
+    })
 };
 
-function fetchCat() {
-    
+function fetchCat(id) {
+    const idCat = `breed_ids=${id}`
+    const urlForSearch = `${BASE_URL}${RESOURSE_CAT}?${idCat}&api_key=${API_KEY}`;
+    return fetch(urlForSearch).then((response) => {
+        return response.json()
+    })
 };
 
-
-export { fetchBreeds };
+export { fetchBreeds, fetchCat };
--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,37 @@
-import { fetchBreeds } from "./cat-api";
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import { fetchBreeds, fetchCat } from "./cat-api";
 
 const elements = {
     selectBreeds: document.querySelector('.breed-select'),
     loader: document.querySelector('.loader'),
     errorMessage: document.querySelector('.error'),
     cardCat: document.querySelector('.cat-info'),
+    backdrop: document.querySelector('.backdrop')
 };
 
-console.log(fetchBreeds());
+elements.selectBreeds.hidden = true;
+elements.errorMessage.hidden = true;
 
-fetchBreeds().then((data) => {
-    console.log(data);
-    elements.errorMessage.hidden = true;
-    elements.selectBreeds.innerHTML = createMarkupBreeds(data);
-}).catch(error => console.log('ERROR axaxaxa'));
+fetchBreeds()
+    .then((data) => {
+        elements.selectBreeds.innerHTML = createMarkupBreeds(data);
+        new SlimSelect({
+            select: '.breed-select'
+        });
+        elements.selectBreeds.hidden = false;
+        elements.backdrop.classList.toggle('is-hidden')
+    })
+    .catch(error => {
+        elements.errorMessage.hidden = false;
+        Report.failure(
+            'ERROR LOADING',
+            'TRY RELOADING THE PAGE',
+            'OK',
+        );
+        elements.backdrop.classList.toggle('is-hidden');
+    });
 
 function createMarkupBreeds(arr) {
     return arr.map(({ id, name }) => {
@@ -21,6 +39,27 @@ function createMarkupBreeds(arr) {
     }).join('');
 };
 
+elements.selectBreeds.addEventListener('change', handlerSelectCat);
 
+function handlerSelectCat(event) {
+    fetchCat(event.target.value)
+        .then((data) => {
+            elements.cardCat.innerHTML = createCardMarkup(data);
+            elements.cardCat.classList.add('js-card-style')
+        })
+        .catch(error => {
+            Report.failure(
+                'ERROR LOADING',
+                'TRY AGAIN OR SELECT ANOTHER CAT',
+                'OK',
+            )
+        });
+};
 
-
+function createCardMarkup(arr) {
+    return arr.map(({ url, breeds: [{ name, description, temperament }] }) => {
+        return `<img src="${url}" alt="cat breed ${name}" width="100%">
+      <h3>Breed:</h3><p>${name}</p><h4>Description:</h4><p>${description}</p>
+      <h5>Temperament:</h5><p>${temperament}</p>`
+    }).join();
+};
